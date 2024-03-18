00d07e34dd09fadecbe5cb39e6bd9319f9d51871
Vaibhav <sukalevaibhav53@gmail.com>
2024-03-18 22:46:35 +0530

    updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { restaurantList } from "../components/constants";
 import RestaurantCard from './RestaurantCard';
 import {useState} from 'react';
+import { swiggy_api_URL } from '../components/constants';
+
+//Filter the restaurant data according input type
 
 function filterData(searchText, restaurants) {
   const filterData = restaurants.filter((restaurant) =>
@@ -10,10 +13,28 @@ function filterData(searchText, restaurants) {
   return filterData;
 }
 
+// Body components for body section: It contain all restaurant cards
 const Body = () => {
   //useState: To create a state variable, searchText is local state variable
   const [searchText, setSearchText] = useState("");
-  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [allrestaurants, setAllRestaurants] = useState([]);
+
+  const [filterRestaurants, setFilterRestaurants] = useState([]);
+
+  //Use useEffect for one time call getRestaurants using empty dependency array 
+
+  useEffect(()=>{
+    getRestuarants();
+  }, []);
+
+  // async function getRestaurant to fetch swiggy API data.
+
+  async function getRestuarants(){
+    const data = await fetch(swiggy_api_URL);
+    const json = await data.json();
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilterRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
   return (
     <>
       <div className="search-container">
@@ -29,9 +50,9 @@ const Body = () => {
           className="search-btn"
           onClick={() => {
             // filter the data
-            const data = filterData(searchText, restaurants);
+            const data = filterData(searchText, allrestaurants);
             // update the state of restaurants list
-            setRestaurants(data);
+            setAllRestaurants(data);
           }}
         >
           Search
